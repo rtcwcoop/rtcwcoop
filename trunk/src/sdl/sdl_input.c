11d03318b400658fdbb8c7cfa45b0e17d9ccd097
frederik.delaere@gmail.com <frederik.delaere@gmail.com@85fc250e-98da-7c7f-6d3a-a8f208e801ef>
2011-02-21 17:46:53 +0000

    debug print

--- trunk/src/sdl/sdl_input.c
+++ trunk/src/sdl/sdl_input.c
@@ -429,7 +429,7 @@ static void IN_ActivateMouse( void )
 			{
 				if(IOHIDGetAccelerationWithKey(mouseDev, CFSTR(kIOHIDMouseAccelerationType), &originalMouseSpeed) == kIOReturnSuccess)
 				{
-					Com_Printf("previous mouse acceleration: %f\n", originalMouseSpeed);
+					Com_DPrintf("previous mouse acceleration: %f\n", originalMouseSpeed);
 					if(IOHIDSetAccelerationWithKey(mouseDev, CFSTR(kIOHIDMouseAccelerationType), -1.0) != kIOReturnSuccess)
 					{
 						Com_Printf("Could not disable mouse acceleration (failed at IOHIDSetAccelerationWithKey).\n");
@@ -509,7 +509,7 @@ static void IN_DeactivateMouse( void )
 			io_connect_t mouseDev = IN_GetIOHandle();
 			if(mouseDev != 0)
 			{
-				Com_Printf("restoring mouse acceleration to: %f\n", originalMouseSpeed);
+				Com_DPrintf("restoring mouse acceleration to: %f\n", originalMouseSpeed);
 				if(IOHIDSetAccelerationWithKey(mouseDev, CFSTR(kIOHIDMouseAccelerationType), originalMouseSpeed) != kIOReturnSuccess)
 					Com_Printf("Could not re-enable mouse acceleration (failed at IOHIDSetAccelerationWithKey).\n");
 				IOServiceClose(mouseDev);
