57fb5d080417a8f9317de41d6384e49a99a49cbb
frederik.delaere@gmail.com <frederik.delaere@gmail.com@85fc250e-98da-7c7f-6d3a-a8f208e801ef>
2011-10-28 10:19:17 +0000

    win32bug

--- trunk/src/sys/sys_win32.c
+++ trunk/src/sys/sys_win32.c
@@ -87,7 +87,7 @@ char *Sys_DefaultHomePath( void )
 			return NULL;
 		}
 		Q_strncpyz( homePath, szPath, sizeof( homePath ) );
-		Q_strcat( homePath, sizeof( homePath ), "\\Quake3" );
+		Q_strcat( homePath, sizeof( homePath ), "\\Wolfenstein" );
 		FreeLibrary(shfolder);
 	}
 
