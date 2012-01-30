c8fca50fced5a049882b7da8ba46d81974dde59e
frederik.delaere@gmail.com <frederik.delaere@gmail.com@85fc250e-98da-7c7f-6d3a-a8f208e801ef>
2012-01-30 17:45:15 +0000

    better version

--- trunk/create_pk3.bat
+++ trunk/create_pk3.bat
@@ -1,6 +1,4 @@
 SET PATH=%CD%;%PATH%
-echo %PATH%
 cd media\sp_pak_coop1\
-zip.exe -r sp_pak_coop1.pk3 models\players\multi\ models\movespeeds\ maps\ scripts\ ui\ . levelshots
+zip.exe -r sp_pak_coop1.pk3 .\ "*.svn*"
 move sp_pak_coop1.pk3 ..\..\
-pause
