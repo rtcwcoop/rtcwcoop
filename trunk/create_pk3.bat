4f3bda6981cc4af5307c08470abc3b6c98f98cc4
frederik.delaere@gmail.com <frederik.delaere@gmail.com@85fc250e-98da-7c7f-6d3a-a8f208e801ef>
2012-01-30 18:49:16 +0000

    pk3script

--- trunk/create_pk3.bat
+++ trunk/create_pk3.bat
@@ -2,3 +2,5 @@ SET PATH=%CD%;%PATH%
 cd media\sp_pak_coop1\
 zip.exe -r sp_pak_coop1.pk3 .\ "*.svn*"
 move sp_pak_coop1.pk3 ..\..\
+cd ..\..\
+copy sp_pak_coop1.pk3 "d:\STEAM\steamapps\common\return to castle wolfenstein\Main\"
