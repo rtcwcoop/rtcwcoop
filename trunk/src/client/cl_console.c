a886405dd54cc3c94b322ce69b0c24704634f6e7
frederik.delaere@gmail.com <frederik.delaere@gmail.com@85fc250e-98da-7c7f-6d3a-a8f208e801ef>
2012-04-10 18:12:37 +0000

    its now possible to draw console when connecting to a server

--- trunk/src/client/cl_console.c
+++ trunk/src/client/cl_console.c
@@ -763,57 +763,28 @@ Con_DrawConsole
 ==================
 */
 void Con_DrawConsole( void ) {
-	// check for console width changes from a vid mode change
-	Con_CheckResize();
 
-	// if disconnected, render console full screen
-	switch ( cls.state ) {
-	case CA_UNINITIALIZED:
-	case CA_CONNECTING:         // sending request packets to the server
-	case CA_CHALLENGING:        // sending challenge packets to the server
-	case CA_CONNECTED:          // netchan_t established, getting gamestate
-	case CA_PRIMED:             // got gamestate, waiting for first frame
-	case CA_LOADING:            // only during cgame initialization, never during main loop
-		if ( !con_debug->integer ) { // these are all 'no console at all' when con_debug is not set
-			return;
-		}
+        // check for console width changes from a vid mode change
+        Con_CheckResize();
 
-		if ( cls.keyCatchers & KEYCATCH_UI ) {
-			return;
-		}
+        // if disconnected, render console full screen
+        if ( cls.state == CA_DISCONNECTED ) { 
+                if ( !( cls.keyCatchers & ( KEYCATCH_UI | KEYCATCH_CGAME ) ) ) { 
+                        Con_DrawSolidConsole( 1.0 );
+                        return;
+                }   
+        }   
 
-		Con_DrawSolidConsole( 1.0 );
-		return;
+        if ( con.displayFrac ) { 
+                Con_DrawSolidConsole( con.displayFrac );
+        } else {
+                // draw notify lines
+                if ( cls.state == CA_ACTIVE ) { 
+                        Con_DrawNotify();
+                }   
+        } 
 
-	case CA_DISCONNECTED:       // not talking to a server
-		if ( !( cls.keyCatchers & KEYCATCH_UI ) ) {
-			Con_DrawSolidConsole( 1.0 );
-			return;
-		}
-		break;
-
-	case CA_ACTIVE:             // game views should be displayed
-		if ( con.displayFrac ) {
-			if ( con_debug->integer == 2 ) {    // 2 means draw full screen console at '~'
-//					Con_DrawSolidConsole( 1.0f );
-				Con_DrawSolidConsole( con.displayFrac * 2.0f );
-				return;
-			}
-		}
-
-		break;
-
-
-	case CA_CINEMATIC:          // playing a cinematic or a static pic, not connected to a server
-	default:
-		break;
-	}
-
-	if ( con.displayFrac ) {
-		Con_DrawSolidConsole( con.displayFrac );
-	} else {
-		Con_DrawNotify();       // draw notify lines
-	}
+        return;
 }
 
 //================================================================
