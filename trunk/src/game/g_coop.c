b6be79f45d1e7d2a6fcd25b8a3b8ceac4266c4c0
frederik.delaere@gmail.com <frederik.delaere@gmail.com@85fc250e-98da-7c7f-6d3a-a8f208e801ef>
2012-08-27 18:36:35 +0000

    removed g_friendlyfire 3, on request by marvin

--- trunk/src/game/g_coop.c
+++ trunk/src/game/g_coop.c
@@ -268,16 +268,16 @@ void Coop_AddStats( gentity_t *targ, gentity_t *attacker, int dmg_ref, int mod )
         // General player stats
         //if ( mod != MOD_SYRINGE ) {
                 if  ( !(targ->r.svFlags & SVF_CASTAI) ) {
-                        if ( g_friendlyFire.integer == 3 ) {
+                        /*if ( g_friendlyFire.integer == 3 ) {
                                 attacker->client->sess.damage_given -= (dmg*2);
                                 if (g_gametype.integer != GT_COOP_BATTLE)
                                         attacker->client->ps.persistant[PERS_SCORE] -= (dmg*2);
-                        } else {
+                        } else {*/
                                 attacker->client->sess.damage_given -= dmg;
                                 if (g_gametype.integer != GT_COOP_BATTLE)
                                         attacker->client->ps.persistant[PERS_SCORE] -= dmg;
                                 targ->client->sess.damage_received += dmg;
-                        }
+                        //}
                 } else {
                         attacker->client->sess.damage_given += dmg;
                         if (g_gametype.integer != GT_COOP_BATTLE)
