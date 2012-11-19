8e0a98d9ee1a85e9b43cfef7376a3262f3223036
frederik.delaere@gmail.com <frederik.delaere@gmail.com@85fc250e-98da-7c7f-6d3a-a8f208e801ef>
2012-11-19 17:15:15 +0000

    indent fix and stopstreamingsound call

--- branches/spmp/src/client/cl_cgame.c
+++ branches/spmp/src/client/cl_cgame.c
@@ -629,7 +629,7 @@ int CL_CgameSystemCalls( int *args ) {
 
 //----(SA)	added
 	case CG_S_STOPSTREAMINGSOUND:
-		//S_StopEntStreamingSound( args[1] );
+		S_StopEntStreamingSound( args[1] );
 		return 0;
 //----(SA)	end
 
@@ -952,19 +952,19 @@ int CL_CgameSystemCalls( int *args ) {
 		// doesn't work for COOP mode
 		return SV_GetModelInfo( args[1], VMA( 2 ), VMA( 3 ) );
 
-    // fretn - render to texture
-    case CG_R_RENDERTOTEXTURE:
-        re.RenderToTexture( args[1], args[2], args[3], args[4], args[5] );
-        return 0;
-    //bani
-    case CG_R_GETTEXTUREID:
-        return re.GetTextureId( VMA( 1 ) ); 
-    //bani - flush gl rendering buffers
+        // fretn - render to texture
+        case CG_R_RENDERTOTEXTURE:
+                re.RenderToTexture( args[1], args[2], args[3], args[4], args[5] );
+                return 0;
+        //bani
+        case CG_R_GETTEXTUREID:
+                return re.GetTextureId( VMA( 1 ) ); 
+        //bani - flush gl rendering buffers
 #ifdef LOCALISATION
-    // - NERVE - SMF
-    case CG_TRANSLATE_STRING:
-        CL_TranslateString( VMA( 1 ), VMA( 2 ) ); 
-        return 0;
+        // - NERVE - SMF
+        case CG_TRANSLATE_STRING:
+                CL_TranslateString( VMA( 1 ), VMA( 2 ) ); 
+                return 0;
 #endif
 	default:
 		Com_Error( ERR_DROP, "Bad cgame system trap: %i", args[0] );
