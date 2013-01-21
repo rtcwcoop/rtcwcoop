5eb29fb8af85a0588b36122760eb04af3f394633
frederik.delaere@gmail.com <frederik.delaere@gmail.com@85fc250e-98da-7c7f-6d3a-a8f208e801ef>
2013-01-21 08:46:36 +0000

    remove the warning

--- trunk/src/game/g_coop.h
+++ trunk/src/game/g_coop.h
@@ -8,5 +8,6 @@ void SetCoopSpawnWeapons( gclient_t *client );
 void CoopInfoMessage( gentity_t *ent );
 void CheckCoopStatus( void );
 gentity_t *SelectRandomCoopSpawnPoint( vec3_t origin, vec3_t angles );
+gentity_t *SelectRandomAntiCoopSpawnPoint( vec3_t origin, vec3_t angles );
 void Coop_DeleteStats( int clientnum );
 void Coop_AddStats( gentity_t *targ, gentity_t *attacker, int dmg_ref, int mod );
