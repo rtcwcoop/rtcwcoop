1a875a9ced69e43e762606c166b7b611ba13310f
frederik.delaere@gmail.com <frederik.delaere@gmail.com@85fc250e-98da-7c7f-6d3a-a8f208e801ef>
2013-03-26 17:57:41 +0000

    some more download related changes

--- trunk/src/qcommon/qcommon.h
+++ trunk/src/qcommon/qcommon.h
@@ -230,8 +230,13 @@ void        NET_Sleep( int msec );
 #define MAX_MSGLEN              32768       // max length of a message, which may
 //#define	MAX_MSGLEN				16384		// max length of a message, which may
 // be fragmented into multiple packets
-#define MAX_DOWNLOAD_WINDOW         8       // max of eight download frames
-#define MAX_DOWNLOAD_BLKSIZE        2048    // 2048 byte block chunks
+//#define MAX_DOWNLOAD_WINDOW         8       // max of eight download frames
+//#define MAX_DOWNLOAD_BLKSIZE        2048    // 2048 byte block chunks
+
+#define MAX_DOWNLOAD_WINDOW             48      // ACK window of 48 download chunks. Cannot set this higher, or clients
+                                                // will overflow the reliable commands buffer
+#define MAX_DOWNLOAD_BLKSIZE            1024    // 896 byte block chunks
+
 
 
 /*
