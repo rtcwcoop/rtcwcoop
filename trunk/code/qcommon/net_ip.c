a51b5ca3054f065d9d90bc14064bff4532b7e2fa
frederik.delaere@gmail.com <frederik.delaere@gmail.com@85fc250e-98da-7c7f-6d3a-a8f208e801ef>
2013-12-30 15:17:40 +0000

    when disabling net_enabled (yeah I know, but I'm the primary example that this can happen) the error message was a bit confusing, so I made it less confusing

--- trunk/code/qcommon/net_ip.c
+++ trunk/code/qcommon/net_ip.c
@@ -322,7 +322,13 @@ static qboolean Sys_StringToSockaddr(const char *s, struct sockaddr *sadr, int s
 			return qtrue;
 		}
 		else
+		{
 			Com_Printf("Sys_StringToSockaddr: Error resolving %s: No address of required type found.\n", s);
+			if (net_enabled->integer == 0)
+			{
+				Com_Printf("This probably happens because you've set net_enabled to 0.\n");
+			}
+		}
 	}
 	else
 		Com_Printf("Sys_StringToSockaddr: Error resolving %s: %s\n", s, gai_strerror(retval));
