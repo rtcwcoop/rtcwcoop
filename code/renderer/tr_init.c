6fb10af9d7ca34de86871ee6ece60cbdf3f743d4
M4N4T4RMS@gmail.com <MAN-AT-ARMS@users.noreply.github.com>
2014-03-19 21:19:37 +0000

    Fix setting r_greyscale 2

--- code/renderer/tr_init.c
+++ code/renderer/tr_init.c
@@ -1189,7 +1189,7 @@ void R_Register( void ) {
 	r_ignoreFastPath = ri.Cvar_Get( "r_ignoreFastPath", "1", CVAR_ARCHIVE | CVAR_LATCH );
 
 	r_greyscale = ri.Cvar_Get("r_greyscale", "0", CVAR_ARCHIVE | CVAR_LATCH);
-	ri.Cvar_CheckRange(r_greyscale, 0, 1, qfalse);
+	ri.Cvar_CheckRange(r_greyscale, 0, 2, qfalse);
 
 	//
 	// temporary latched variables that can only change over a restart
