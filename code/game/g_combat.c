be9b623cdc1c3961acdbd0ee00bf0483d6dc088f
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2017-06-27 21:36:45 -0400

    Only add points for hits to live enemies

--- code/game/g_combat.c
+++ code/game/g_combat.c
@@ -1390,7 +1390,7 @@ void G_Damage( gentity_t *targ, gentity_t *inflictor, gentity_t *attacker,
 
 		//G_Printf("health at: %d\n", targ->health);
 		if ( targ->health <= 0 ) {
-			Coop_AddStats( targ, attacker, take, mod );
+			//Coop_AddStats( targ, attacker, take, mod );
 			if ( client ) {
 				targ->flags |= FL_NO_KNOCKBACK;
 			}
