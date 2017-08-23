0db73dabbe42a618cc50010838b8aa93dfa357e5
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2017-08-23 11:30:05 -0400

    Sync iortcw commits
    Sync up to https://github.com/iortcw/iortcw/commit/e422b432c15dab36ed88705ecf532bbee1ea078c

--- code/qcommon/common.c
+++ code/qcommon/common.c
@@ -1536,6 +1536,19 @@ void Com_QueueEvent( int time, sysEventType_t type, int value, int value2, int p
 {
 	sysEvent_t  *ev;
 
+	// combine mouse movement with previous mouse event
+	if ( type == SE_MOUSE && eventHead != eventTail )
+	{
+		ev = &eventQueue[ ( eventHead + MAX_QUEUED_EVENTS - 1 ) & MASK_QUEUED_EVENTS ];
+
+		if ( ev->evType == SE_MOUSE )
+		{
+			ev->evValue += value;
+			ev->evValue2 += value2;
+			return;
+		}
+	}
+
 	ev = &eventQueue[ eventHead & MASK_QUEUED_EVENTS ];
 
 	if ( eventHead - eventTail >= MAX_QUEUED_EVENTS )
@@ -2798,6 +2811,8 @@ void Com_Frame( void ) {
 		else
 			NET_Sleep(timeVal - 1);
 	} while(Com_TimeVal(minMsec));
+
+	IN_Frame();
 	
 	lastTime = com_frameTime;
 	com_frameTime = Com_EventLoop();
