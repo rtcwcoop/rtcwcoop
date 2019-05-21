729f8f88265812914d9b36c01e17131508af13e0
Duko <Duk0@users.noreply.github.com>
2019-05-21 13:37:45 +0200

    dedicated servers can send completed objectives for notebook

--- code/cgame/cg_servercmds.c
+++ code/cgame/cg_servercmds.c
@@ -1234,15 +1234,12 @@ static void CG_MapRestart( void ) {
 	CG_ClearParticles();
 	// done.
 
-	for ( i = 1; i < MAX_PARTICLES_AREAS; i++ )
-	{
-		{
-			int rval;
+	for ( i = 1; i < MAX_PARTICLES_AREAS; i++ ) {
+		int rval;
 
-			rval = CG_NewParticleArea( CS_PARTICLES + i );
-			if ( !rval ) {
-				break;
-			}
+		rval = CG_NewParticleArea( CS_PARTICLES + i );
+		if ( !rval ) {
+			break;
 		}
 	}
 
@@ -1318,6 +1315,12 @@ static void CG_MapRestart( void ) {
 	}
 
 	trap_Cvar_Set( "cg_thirdPerson", "0" );
+
+	if ( !cgs.localServer ) {
+		for ( i = 1; i <= 8; i++ ) {
+			trap_Cvar_Set( va( "g_objective%i", i ), "0" );
+		}
+	}
 }
 
 /*
@@ -1703,13 +1706,18 @@ static void CG_ServerCommand( void ) {
 		// skip the pregame menu and start the game
 		trap_Cvar_Set( "cg_norender", "0" );
 		trap_SendClientCommand( "playerstart" );
-		trap_Cvar_Set( "_pregame", "0" );
+		trap_Cvar_Set( "_pregame", "1" ); // was 0
 		trap_S_FadeAllSound( 1.0f, 1000 ); // fade sound up
 
+#ifdef CGAME_TEST
+		if ( trap_Argc() == 2 && atoi( CG_Argv( 1 ) ) ) {
+			return;
+		}
+		
+		trap_SendClientCommand( "test_objectives" );
+#endif
 		return;
 	}
-
-
 
 	// ensure a file gets into a build (mainly for scripted music calls)
 	if ( !strcmp( cmd, "addToBuild" ) ) {
@@ -1723,6 +1731,27 @@ static void CG_ServerCommand( void ) {
 		//CG_FileTouchForBuild(CG_Argv(1));
 		trap_FS_FOpenFile( CG_Argv( 1 ), &f, FS_READ );
 		trap_FS_FCloseFile( f );
+		return;
+	}
+
+	if ( !strcmp( cmd, "objective" ) ) {
+		if ( cgs.localServer ) {
+			return;
+		}
+		
+		if ( trap_Argc() < 3 ) {
+			return;
+		}
+		
+		int lvl = atoi( CG_Argv( 1 ) );
+		const char *val = CG_Argv( 2 );
+		
+		trap_Cvar_Set( va( "g_objective%i", lvl ), val );
+		
+		if ( atoi( val ) ) {
+			CG_Printf( "Objective %i completed.\n", lvl );
+		}
+		
 		return;
 	}
 
